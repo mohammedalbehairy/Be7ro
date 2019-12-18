c07a386f48c99b2ff106d5fdc58c67746c31eb97
mohammedalbehairy <mohammedalbehairy90@gmail.com>
2019-12-18 21:39:42 +0200

    create needed component

--- be7ro/src/app/app.module.ts
+++ be7ro/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ServicesComponent } from './services/services.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { MyBlogComponent } from './my-blog/my-blog.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ResumeComponent,
+    ServicesComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    MyBlogComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
